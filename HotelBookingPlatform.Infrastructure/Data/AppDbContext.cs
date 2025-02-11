8ac8435bbd4d37b95458336ec0bc33d9197ace4e
John Clayton Blanc <108766949+bjclayton@users.noreply.github.com>
2025-02-10 23:53:18 -0500

    chore: upgrade dependencies to .NET 8 and remove deprecated packages

---
+++ HotelBookingPlatform.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿namespace HotelBookingPlatform.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<LocalUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+
+        public AppDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new HotelConfiguration());
+            modelBuilder.ApplyConfiguration(new RoomClassConfiguration());
+            modelBuilder.ApplyConfiguration(new RoomConfiguration());
+            modelBuilder.ApplyConfiguration(new BookingConfiguration());
+            modelBuilder.ApplyConfiguration(new DiscountConfiguration());
+            modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Owner> owners { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<LocalUser> LocalUsers { get; set; }
+        public DbSet<RoomClass> RoomClasses { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<InvoiceRecord> InvoiceRecords { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
---
+++ HotelBookingPlatform.Infrastructure/Implementation/OwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelBookingPlatform.Infrastructure.Implementation
+{
+    public class OwnerRepository : GenericRepository<Owner>, IOwnerRepository
+    {
+        public OwnerRepository(AppDbContext context)
+            : base(context) { }
+        public async Task<IEnumerable<Owner>> GetAllWithHotelsAsync()
+        {
+            return await _appDbContext.owners
+                .Include(h => h.Hotels)
+                .ToListAsync();
+        }
+    }
+}
