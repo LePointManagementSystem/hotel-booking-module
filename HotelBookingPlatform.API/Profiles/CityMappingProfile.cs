4d70c7d2e004f7267761df4e5cccbb09ce5f328f
John Clayton Blanc <108766949+bjclayton@users.noreply.github.com>
2025-02-13 22:35:44 -0500

    feat: update user registration to support both Admin and User roles

---
+++ HotelBookingPlatform.API/Profiles/CityMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBookingPlatform.API.Profiles;
+public class CityProfile : Profile
+{
+    public CityProfile()
+    {
+
+        CreateMap<City, CityResponseDto>();
+        CreateMap<City, CityWithHotelsResponseDto>()
+            .ForMember(dest => dest.Hotels, opt => opt.MapFrom(src => src.Hotels));
+
+        CreateMap<CityCreateRequest, City>();
+
+        CreateMap<Hotel, HotelBasicResponseDto>();
+    }
+}
