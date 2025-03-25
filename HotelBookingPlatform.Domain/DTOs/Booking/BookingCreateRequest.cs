199677e101883b1693aabd96f1b95d7959a2e243
jantoine2 <antoinejovany99@gmail.com>
2025-03-25 14:00:55 -0400

    Let the Staff to create Booking for Client

--- HotelBookingPlatform.Domain/DTOs/Booking/BookingCreateRequest.cs
+++ HotelBookingPlatform.Domain/DTOs/Booking/BookingCreateRequest.cs
@@ -1,5 +1,12 @@
 ﻿namespace HotelBookingPlatform.Domain.DTOs.Booking
 {
+    public class GuestDto {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string CIN { get; set; }
+    }
+
     public class BookingCreateRequest
     {
         public int HotelId { get; set; }
@@ -7,5 +14,7 @@
         public DateTime CheckOutDateUtc { get; set; }
         public ICollection<int> RoomIds { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
+
+        public GuestDto Guest { get; set; }
     }
 }
