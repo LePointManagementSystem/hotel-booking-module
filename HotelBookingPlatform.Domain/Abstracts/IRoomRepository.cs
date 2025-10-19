03830ea45ea48be421982909437134576789552f
jantoine2 <antoinejovany99@gmail.com>
2025-10-19 09:54:52 -0400

    Release booking with id

--- HotelBookingPlatform.Domain/Abstracts/IRoomRepository.cs
+++ HotelBookingPlatform.Domain/Abstracts/IRoomRepository.cs
@@ -3,6 +3,8 @@
     public interface IRoomRepository : IGenericRepository<Room>
     {
         public Task<Room> GetByIdAsync(int id);
+        //Task UpdateRangeAsync(IEnumerable<Room> rooms);
+
         Task<IEnumerable<Room>> GetRoomsByPriceRangeAsync(decimal minPrice, decimal maxPrice);
         Task<IEnumerable<Room>> GetAvailableRoomsWithNoBookingsAsync(int roomClassId);
         Task<IEnumerable<Room>> GetRoomsAvailableBetweenDatesAsync(int roomClassId, DateTime checkIn, DateTime checkOut);
